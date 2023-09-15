2714f2267bdce7fa0a4dc9aa2676ea50705ab4f2
Muneeza Leslie <sebagabomuneeza@gmail.com>
2023-09-15 14:23:10 +0300

    create crud operation for user

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const moment = require("moment");
 const app = express();
+
+const userRoutes = require("./src/routes/user");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const uri = process.env.MONGO_URI || "mongodb://127.0.0.1:27017/hng";
+
+mongoose
+  .connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Successfully connected to the database");
+  })
+  .catch((err) => {
+    console.log("Could not connect to the database. Error...", err);
+    process.exit();
+  });
+
+app.use("/users", userRoutes);
+
 const port = process.env.PORT || 3000;
 
 app.get("/hng/api", (req, res) => {
